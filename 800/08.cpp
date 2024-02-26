73f30896c81e39d54e4793261c08739db13ff1d3
VanshRana-1004 <130490295+VanshRana-1004@users.noreply.github.com>
2024-02-26 22:42:36 +0530

    Create 08.cpp

---
+++ 800/08.cpp
@@ -0,0 +1,19 @@
+// Problem Link : https://codeforces.com/problemset/problem/1878/A
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--){
+        int x,n,k;
+        cin>>n>>k;
+        unordered_map<int,int> mp;
+        while(n--){
+            cin>>x;
+            mp[x]++;
+        }
+        if(mp.find(k)!=mp.end()) cout<<"YES"<<endl;
+        else cout<<"NO"<<endl;
+    }
+}
