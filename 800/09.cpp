7df0a1de2fe5702c1a582b08df1596c870a5175c
VanshRana-1004 <130490295+VanshRana-1004@users.noreply.github.com>
2024-02-26 22:43:37 +0530

    Create 09.cpp

---
+++ 800/09.cpp
@@ -0,0 +1,19 @@
+// Problem Link : https://codeforces.com/problemset/problem/1877/A
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--){
+        int s1=0,s2=0,n,x;
+        cin>>n;
+        n--;
+        while(n--){
+            cin>>x;
+            if(x<0) s1+=(-1)*x;
+            else s2+=x;
+        }
+        cout<<s1-s2<<endl;
+    }
+}
