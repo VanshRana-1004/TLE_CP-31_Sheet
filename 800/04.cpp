41e0176588321d7eb4e0f18df359ad740da7e497
VanshRana-1004 <130490295+VanshRana-1004@users.noreply.github.com>
2024-02-26 22:38:35 +0530

    Create 04.cpp

---
+++ 800/04.cpp
@@ -0,0 +1,14 @@
+// Problem Link : https://codeforces.com/problemset/problem/1899/A
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--){
+        int n;
+        cin>>n;
+        if(n%3!=0) cout<<"First"<<endl;
+        else cout<<"Second"<<endl;
+    }
+}
