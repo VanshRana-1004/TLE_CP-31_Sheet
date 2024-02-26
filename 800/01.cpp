f985ff114dd8a9d418b92ec399dfd179a76cbb0e
VanshRana-1004 <130490295+VanshRana-1004@users.noreply.github.com>
2024-02-26 22:36:18 +0530

    Update 01.cpp

--- 800/01.cpp
+++ 800/01.cpp
@@ -1,4 +1,4 @@
-Problem Link : https://codeforces.com/problemset/problem/1903/A
+// Problem Link : https://codeforces.com/problemset/problem/1903/A
 #include <bits/stdc++.h>
 using namespace std;
 int main()
