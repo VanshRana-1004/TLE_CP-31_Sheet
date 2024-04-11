46c477bfce3961d080557e3729f9cef25e58ea29
VanshRana-1004 <130490295+VanshRana-1004@users.noreply.github.com>
2024-04-11 18:27:26 +0530

    Cardboard for Pictures

--- 1100/09.cpp
+++ 1100/09.cpp
@@ -1,26 +1,30 @@
 // Problem Link : https://codeforces.com/contest/1850/problem/E
 #include <bits/stdc++.h>
 using namespace std;
-#define ll unsigned long long int
+#define ll long long 
+#define endl "\n"
 int main()
 {
     int t;
     cin>>t;
     while(t--){
-        ll n,c,i,ans=-1;
+        ll n,c,i;
         cin>>n>>c;
-        vector<ll> size(n);
-        for(i=0;i<n;i++) cin>>size[i];
-        ll s=1,e=c,mid;
+        vector<ll> store(n);
+        for(i=0;i<n;i++) cin>>store[i];
+        ll s=1,e=1e9,mid,ans=-1;
         while(s<=e){
             mid=s+(e-s)/2;
             ll area=0;
-            for(i=0;i<n && area<=c;i++) area+=(size[i]+(mid*2))*(size[i]+(mid*2));
-            if(i==n && area==c){
+            for(i=0;i<n;i++){
+                area+=(store[i]+2*mid)*(store[i]+2*mid);
+                if(area>c) break;
+            } 
+            if(area==c){
                 ans=mid;
                 break;
-            } 
-            else if(i==n && area<c) s=mid+1;
+            }
+            if(area<c) s=mid+1;
             else e=mid-1;
         }
         cout<<ans<<endl;
