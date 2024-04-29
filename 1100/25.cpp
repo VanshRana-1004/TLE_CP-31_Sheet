87244e00f8770fc64821df090852520bc56b7aad
VanshRana-1004 <130490295+VanshRana-1004@users.noreply.github.com>
2024-04-29 20:08:09 +0530

    A Perfectly Balanced String?

---
+++ 1100/25.cpp
@@ -0,0 +1,48 @@
+// Problem Link : https://codeforces.com/contest/1673/problem/B
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long int
+#define uni unsigned long long int
+#define pb(x) push_back(x)
+#define sz(arr) arr.size()
+#define ssz(s) s.length()
+#define nl endl
+#define read(arr,n) for(ll i=0;i<n;i++) cin>>arr[i]
+#define srt(arr) sort(arr.begin(),arr.end())
+#define srtr(arr) sort(arr.rbegin(),arr.rend())
+#define rev(arr) reverse(arr.begin(),arr.end())
+#define pi pair<int,int>
+#define pl pair<ll,ll>
+#define vi vector<int>
+#define vll vector<ll>
+#define vii vector<vector<int>>
+#define vlll vector<vector<ll>>
+#define mn(a) *min_element(a.begin(),a.end())
+#define mx(a) *max_element(a.begin(),a.end())
+#define vs vector<string>
+#define vss vector<vector<string>>
+#define vc vector<char>
+#define vcc vector<vector<char>>
+#define pre_sum(pre,arr,n) for(i=1;i<n;i++) pre[i]=pre[i-1]+arr[i]
+#define suf_sum(suf,arr,n) for(i=n-2;i>=0;i--) suf[i]=suf[i+1]+arr[i]
+const double pie=3.14159265358979323846264338327950;
+const ll mod=1e9+7;
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--){
+        string s,ans="YES";
+        cin>>s;
+        int i,n=s.length();
+        set<char> st;
+        for(i=0;i<n;i++) st.insert(s[i]);
+        for(i=0;i<n-st.size();i++){
+            if(s[i]!=s[i+st.size()]){
+                ans="NO";
+                break;
+            } 
+        } 
+        cout<<ans<<nl;       
+    }    
+}
