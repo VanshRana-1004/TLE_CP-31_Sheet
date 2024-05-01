fd5c95def2c3bfbd861ee6e7867988cc966191d0
VanshRana-1004 <130490295+VanshRana-1004@users.noreply.github.com>
2024-05-01 12:20:57 +0530

    Paint the Array

---
+++ 1100/29.cpp
@@ -0,0 +1,65 @@
+// Problem Link : https://codeforces.com/contest/1618/problem/C
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long int
+#define uni unsigned long long int
+#define pb(x) push_back(x)
+#define sz(arr) arr.size()
+#define ssz(s) s.length()
+#define nl endl
+#define read(arr,n) for(ll i=0;i<n;i++) cin>>arr[i]
+#define srt(arr) sort(arr.begin(),arr.end())
+#define srtr(arr) sort(arr.rbegin(),arr.rend())
+#define rev(arr) reverse(arr.begin(),arr.end())
+#define pi pair<int,int>
+#define pl pair<ll,ll>
+#define vi vector<int>
+#define vll vector<ll>
+#define vii vector<vector<int>>
+#define vlll vector<vector<ll>>
+#define mn(a) *min_element(a.begin(),a.end())
+#define mx(a) *max_element(a.begin(),a.end())
+#define vs vector<string>
+#define vss vector<vector<string>>
+#define vc vector<char>
+#define vcc vector<vector<char>>
+#define pre_sum(pre,arr,n) for(i=1;i<n;i++) pre[i]=pre[i-1]+arr[i]
+#define suf_sum(suf,arr,n) for(i=n-2;i>=0;i--) suf[i]=suf[i+1]+arr[i]
+const double pie=3.14159265358979323846264338327950;
+const ll mod=1e9+7;
+ll gcd(ll a,ll b){
+    while(a!=0 && b!=0){
+        if(a>b) a%=b;
+        else b%=a;
+    }
+    if(a==0) return b;
+    return a;
+}
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--){
+        ll n,i,a=-1,b=-1,flag1=0,flag2=0;
+        cin>>n;
+        vll arr(n);
+        read(arr,n);
+        for(i=0;i<n;i++){
+            if(i%2==0){
+                if(a==-1) a=arr[i];
+                else a=gcd(a,arr[i]);
+            }
+            else{
+                if(b==-1) b=arr[i];
+                else b=gcd(b,arr[i]);
+            }
+        }
+        for(i=0;i<n;i++){
+            if(i%2==0 && arr[i]%b==0) flag1=1;
+            if(i%2==1 && arr[i]%a==0) flag2=1;
+        }
+        if(flag1==0) cout<<b<<nl;
+        else if(flag2==0) cout<<a<<nl;
+        else cout<<0<<nl;
+    }    
+}
